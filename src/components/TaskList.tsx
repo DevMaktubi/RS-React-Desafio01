b6ce485fef041d4d8f1288ab3d15ceefe7bda134
devmaktubi <brenoluiz1@gmail.com>
2022-01-17 09:34:38 -0300

    Resolvido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.length < 3){
+      alert("O título da task deve ter mais que 3 caracteres.")
+      return
+    }
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(
+      task => task.id === id ? {
+        ...task,
+        isComplete: !task.isComplete
+      } : task
+    )
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(t => t.id !== id);
+    setTasks(filteredTasks)
   }
 
   return (
